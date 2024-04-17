902ed887195614d16d91e09700de52514cdfc462
oheil <git@heilbit.de>
2024-04-17 03:54:23 +0200

    add option to show standard library packages (#12)

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,14 +5,16 @@ import Term: Tree
 @testset "Make sure function runs without error" begin
     for dedup in [true; false]
     for compat in [true; false]
-        @test PkgDependency.tree(; compat, dedup) isa Tree
-        @test PkgDependency.tree("Term"; compat, dedup) isa Tree
-        @test PkgDependency.tree("Term"; reverse=true, compat, dedup) isa Tree
+    for stdlib in [true; false]
+        @test PkgDependency.tree(; compat, dedup, stdlib) isa Tree
+        @test PkgDependency.tree("Term"; compat, dedup, stdlib) isa Tree
+        @test PkgDependency.tree("Term"; reverse=true, compat, dedup, stdlib) isa Tree
         if VERSION < v"1.7"
-            @test_throws ErrorException PkgDependency.tree("Term"; compat, show_link=true, dedup) isa Tree
+            @test_throws ErrorException PkgDependency.tree("Term"; compat, show_link=true, dedup, stdlib) isa Tree
         else
-            @test PkgDependency.tree("Term"; compat, show_link=true, dedup) isa Tree
+            @test PkgDependency.tree("Term"; compat, show_link=true, dedup, stdlib) isa Tree
         end
+    end
     end
     end
 end
