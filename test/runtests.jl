3728d0526dba68090ae6afff04a64ec3e99c1233
Reuben Gardos Reid <5456207+ReubenJ@users.noreply.github.com>
2024-05-19 11:02:38 +0200

    Allow for arbitrary `kwargs` in `tree(...)` (#13)
    * Allow for arbitrary `kwargs` in `tree(...)`
    
    This change allows for users to control features like the `maxdepth` of the tree.
    It is also a more robust solution than, for example, simply adding a `maxdepth`
    argument to `tree(...)` itself.
    
    * Update documentation to reflect arbitrary kwargs feature

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,14 +6,16 @@ import Term: Tree
     for dedup in [true; false]
     for compat in [true; false]
     for stdlib in [true; false]
-        @test PkgDependency.tree(; compat, dedup, stdlib) isa Tree
-        @test PkgDependency.tree("Term"; compat, dedup, stdlib) isa Tree
-        @test PkgDependency.tree("Term"; reverse=true, compat, dedup, stdlib) isa Tree
+    for maxdepth in [99; 1; 2]
+        @test PkgDependency.tree(; compat, dedup, stdlib, maxdepth) isa Tree
+        @test PkgDependency.tree("Term"; compat, dedup, stdlib, maxdepth) isa Tree
+        @test PkgDependency.tree("Term"; reverse=true, compat, dedup, stdlib, maxdepth) isa Tree
         if VERSION < v"1.7"
-            @test_throws ErrorException PkgDependency.tree("Term"; compat, show_link=true, dedup, stdlib) isa Tree
+            @test_throws ErrorException PkgDependency.tree("Term"; compat, show_link=true, dedup, stdlib, maxdepth) isa Tree
         else
-            @test PkgDependency.tree("Term"; compat, show_link=true, dedup, stdlib) isa Tree
+            @test PkgDependency.tree("Term"; compat, show_link=true, dedup, stdlib, maxdepth) isa Tree
         end
+    end
     end
     end
     end
